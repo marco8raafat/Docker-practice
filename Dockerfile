180d43f339d88aba4507ef08ef625d7aef111420
Marco <marco782005@gmail.com>
2025-09-08 15:19:02 +0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:22 as base
+#base stage
+
+
+FROM base as development
+#will take the result of base stage and will continue to it (add to it) development stage.
+WORKDIR /app
+COPY package.json .
+RUN npm install
+COPY . .
+EXPOSE 4000
+CMD ["npm", "run","start-dev"]
+
+
+FROM base as production
+#will take the result of base stage and will continue to it (add to it) production stage.
+WORKDIR /app
+COPY package.json .
+RUN npm install --only=production 
+COPY . .
+EXPOSE 4000
+CMD ["npm", "start"]
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose')
+
+// Initialize app
+const PORT = 4000;
+const app = express();
+
+//connect to mongodb
+const DB_USER = 'root';
+const DB_PASSWORD =  'example';
+const DB_PORT = 27017;
+const DB_HOST = 'mongo'; // use the service name from docker-compose.yml instead of the IP address (docker will map the hostname and put the IP address automatically)
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose.connect(URI).then(() => {
+    console.log('MongoDB connected');
+}).catch((err) => console.log('failed to connect to db: ',err));
+
+app.get('/', (req, res) => res.send('<h1> Hello Tresmerge! hiii </h1>'));
+
+app.listen(PORT, () => console.log(`app is up and running on port: ${PORT}`));
